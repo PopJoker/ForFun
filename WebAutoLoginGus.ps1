5113300b1d1b9e5390382299852d4605f148a86a
maverick <maverick.tu@gus-tech.com.tw>
2025-12-17 11:04:18 +0800

    2025-12-17

--- WebAutoLoginGus.ps1
+++ WebAutoLoginGus.ps1
@@ -1,23 +1,25 @@
 # 等待系統穩定
 Start-Sleep -Seconds 10
 
-# 設定要打開的網址
-$url = "https://webems.gustech.com.tw/"
-
 # Chrome 路徑
 $chromePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 
 # 使用 Chrome 開啟網址，無痕模式，全螢幕
-Start-Process $chromePath -ArgumentList "--incognito $url"
+Start-Process $chromePath -ArgumentList @(
+    "--profile-directory=Default",
+    "--incognito",
+    "--start-maximized",
+    "https://webems.gustech.com.tw/"
+)
+
 # 等待瀏覽器載入
 Start-Sleep -Seconds 5
-
-[System.Windows.Forms.SendKeys]::SendWait("{F11}")
 
 # 自動填入帳號密碼 (用 SendKeys 模擬)
 Add-Type -AssemblyName System.Windows.Forms
 
 # 模擬 Tab/Enter 填寫表單
+[System.Windows.Forms.SendKeys]::SendWait("{F11}")
 [System.Windows.Forms.SendKeys]::SendWait("{TAB}")
 [System.Windows.Forms.SendKeys]::SendWait("GusOfficeTest@gus-tech.com.tw")
 [System.Windows.Forms.SendKeys]::SendWait("{TAB}")
